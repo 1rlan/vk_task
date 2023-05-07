66bc5f2ed28f65f9f48d4eaca210e5ee67dedf9a
1rlan <irlanidzzze@gmail.com>
2023-05-08 00:46:26 +0200

    ui: added overlay + burqa

---
+++ ticTacToe/ticTacToe/Game/GameFinish.swift
@@ -0,0 +1,8 @@
+//
+//  GameFinish.swift
+//  ticTacToe
+//
+//  Created by Ирлан Абушахманов on 08.05.2023.
+//
+
+import Foundation
---
+++ ticTacToe/ticTacToe/Game/OverlayView.swift
@@ -0,0 +1,20 @@
+//
+//  Overlat.swift
+//  ticTacToe
+//
+//  Created by Ирлан Абушахманов on 08.05.2023.
+//
+
+import SwiftUI
+
+struct Overlat: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Overlat_Previews: PreviewProvider {
+    static var previews: some View {
+        Overlat()
+    }
+}
